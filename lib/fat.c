538f28094b583db73995fd5836154e890ae11803
Raine <git@chaoticgoose.com>
2024-07-29 00:38:33 +1000

    feat: added FSInfo Sector writer

--- lib/fat.c
+++ lib/fat.c
@@ -16,7 +16,7 @@
 #define SECTOR_SIZE             512
 #define SECTORS_PER_FAT         1 // FIXME: This is a placeholder
 #define ROOT_CLUSTER            2
-#define FS_INFO_OFFSET_32       2
+#define FS_INFO_OFFSET_32       1
 #define BOOT_SIGNATURE          0xAA55
 #define BOOTSECTOR_SIGNATURE    0x29
 #define MEDIA_TYPE              0xF8
@@ -25,13 +25,19 @@
 #define FAT16_FS_TYPE "FAT16   "
 #define FAT12_FS_TYPE "FAT12   "
 
+#define FSINFO_LEAD_SIGNATURE 0x41615252
+#define FSINFO_STRUCT_SIGNATURE 0x61417272
+#define FSINFO_TRAIL_SIGNATURE 0xAA550000
+#define FSINFO_UNKNOWN 0xFFFFFFFF
+
 const uint8_t FAT32_JMP_INSTRUCTION[3] = {0xEB, 0x58, 0x90};
 const uint8_t FAT12_16_JMP_INSTRUCTION[3] = {0xEB, 0x3C, 0x90};
 
 static ssize_t zero_disk(int* fd);
-static ssize_t boot_sector(
-        int* fd, uint8_t FATVersion, uint8_t clusterSectors, char volumeLabel[11]);
+static ssize_t boot_sector(int* fd, uint8_t FATVersion, uint8_t clusterSectors, char volumeLabel[11]);
+static ssize_t writeFsInfo(int* fd, uint16_t fsInfoSector);
 
+// Binary Ganerated from src/boot.asm
 uint8_t bootCode12_16[FAT12_16_BOOTCODE_SIZE] = {
     0xBE, 0x00, 0x00, 0xB4, 0x0E, 0x8A, 0x84, 0x19, 0x7C, 0xCD, 0x10, 0x83, 0xC6, 0x01, 0x80, 
     0xBC, 0x19, 0x7C, 0x00, 0x75, 0xEE, 0xCD, 0x18, 0xEB, 0xFE, 0x54, 0x68, 0x69, 0x73, 0x20, 
@@ -45,6 +51,16 @@ uint8_t bootCode32[FAT32_BOOTCODE_SIZE] = {
     0x69, 0x73, 0x20, 0x6E, 0x6F, 0x74, 0x20, 0x61, 0x20, 0x62, 0x6F, 0x6F, 0x74, 0x61, 0x62, 
     0x6C, 0x65, 0x20, 0x64, 0x69, 0x73, 0x6B, 0x0A, 0x0D
 };
+
+struct fsInfo {
+    uint32_t lead_signiture; /* 0x41615252, Confirms that this is a fsInfo sector */
+    uint8_t reserved[480]; /* unused */
+    uint32_t struct_signiture; /* 0x61417272, Another struct signiture */
+    uint32_t free_clusters; /* Number of free clusters on the disk */
+    uint32_t next_cluster; /* Next free cluster */
+    uint8_t reserved2[12]; /* unused */
+    uint32_t trail_signiture; /* 0xAA550000 */
+} __attribute__((packed));
 
 struct msdos_volume_info {
     uint8_t drive_number; /* BIOS drive number */
@@ -109,6 +125,11 @@ int fat_format(char* target, int fatVersion, uint8_t clusterSectors, char volume
     printf("clusterSize: %d\n", clusterSectors);
     boot_sector(&fd, fatVersion, clusterSectors, volumeLabel);
 
+    if (fatVersion == 32)
+    {
+        writeFsInfo(&fd, FS_INFO_OFFSET_32);
+    }
+
     return 0;
 }
 
@@ -130,6 +151,31 @@ static ssize_t zero_disk(int* fd)
     return 0;
 }
 
+static ssize_t writeFsInfo(int* fd, uint16_t fsInfoSector)
+{
+    struct fsInfo* fsInfo = malloc(sizeof(struct fsInfo));
+    // zero out reserved
+    memset(fsInfo->reserved, 0, 480);
+    memset(fsInfo->reserved2, 0, 12);
+    fsInfo->lead_signiture = FSINFO_LEAD_SIGNATURE;
+    fsInfo->struct_signiture = FSINFO_STRUCT_SIGNATURE;
+    fsInfo->free_clusters = FSINFO_UNKNOWN;
+    fsInfo->next_cluster = FSINFO_UNKNOWN;
+    fsInfo->trail_signiture = FSINFO_TRAIL_SIGNATURE;
+
+    /* Write fsInfo to disk */
+    lseek(*fd, fsInfoSector * SECTOR_SIZE, SEEK_SET);
+    int status = write(*fd, fsInfo, sizeof(struct fsInfo));
+    if (status == -1)
+    {
+        perror("write");
+        return -1;
+    }
+
+    free(fsInfo);
+    return 0;
+}
+
 static ssize_t boot_sector(
         int* fd, uint8_t FATVersion, uint8_t clusterSectors, char volumeLabel[11])
 {
@@ -139,7 +185,6 @@ static ssize_t boot_sector(
     struct hd_geometry geo = get_bios_params(fd);
 
     /* Set boot sector values */
-
     struct msdos_boot_sector* bootSector = malloc(sizeof(struct msdos_boot_sector));
     memcpy(bootSector->OEM_name, OEM_NAME, 8);
     bootSector->sector_bytes = SECTOR_SIZE;
